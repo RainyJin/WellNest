bbc125901e1efc67df26dcb6fbda0575928e25cf
Charlotte Wan <zwan35@wisc.edu>
2024-12-07 23:01:57 -0600

    Implemented (unilateral) conversation function - pet is able to give out a message

--- app/src/main/java/com/cs407/wellnest/PetProfileFragment.kt
+++ app/src/main/java/com/cs407/wellnest/PetProfileFragment.kt
@@ -4,19 +4,16 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Looper
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Text
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Face
-import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.MailOutline
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -34,17 +31,15 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.core.app.ActivityCompat
 import androidx.navigation.NavController
-import coil.compose.rememberAsyncImagePainter
-import coil.decode.ImageDecoderDecoder
-import coil.request.ImageRequest
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.cs407.wellnest.data.birdData
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.Priority
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+import com.cs407.wellnest.data.birdData
+import com.cs407.wellnest.data.messages
+import kotlin.random.Random
 
 @SuppressLint("ServiceCast")
 @Composable
@@ -54,6 +49,7 @@ fun PetProfileScreen(navController: NavController) {
 
     var location by remember { mutableStateOf<Pair<Double, Double>?>(null) }
     var birdNameDescPair by remember { mutableStateOf<Pair<String, String>?>(null) }
+    var message by remember { mutableStateOf("Hi! How are you today?") }
 
     val locationRequest = LocationRequest.Builder(
         Priority.PRIORITY_HIGH_ACCURACY, // priority
@@ -68,9 +64,7 @@ fun PetProfileScreen(navController: NavController) {
             val locationData = locationResult.lastLocation
             if (locationData != null) {
                 location = Pair(locationData.latitude, locationData.longitude)
-                Log.d("Location", "Updated location: (${locationData.latitude}, ${locationData.longitude})")
             } else {
-                Log.d("Error", "LocationCallback returned null")
                 location = Pair(38.0, 106.0) // default location
             }
         }
@@ -87,18 +81,7 @@ fun PetProfileScreen(navController: NavController) {
         fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
     }
 
-    if (location == null) {Log.d("Error", "location is null")}
-    location?.let{
-        Log.d("lat", "${it.first}")
-        Log.d("long", "${it.second}")
-    }
-
     birdNameDescPair = location?.let { getBirdNameAndDescription(it.first, it.second) }
-    if (birdNameDescPair == null) {Log.d("Error", "birdNameDescPair is null")}
-    birdNameDescPair?.let {
-        Log.d("Bird Name", it.first)
-        Log.d("Bird Des", it.second)
-    }
 
     // stop location updates
     DisposableEffect(Unit) {
@@ -107,114 +90,141 @@ fun PetProfileScreen(navController: NavController) {
         }
     }
 
-    Column(
+    Box(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
+            .fillMaxWidth()
     ) {
-        Row(
+        IconButton(
+            onClick = { navController.popBackStack() },
             modifier = Modifier
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
+                .align(Alignment.TopStart)
+                .padding(10.dp)
         ) {
-            IconButton(
-                onClick = { navController.popBackStack() }
+            Icon(
+                Icons.AutoMirrored.Filled.ArrowBack,
+                contentDescription = "Back",
+                tint = Color.Black
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .padding(top = 32.dp)
+                .verticalScroll(rememberScrollState()) // Enable scrolling
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
             ) {
-                Icon(
-                    Icons.AutoMirrored.Filled.ArrowBack,
-                    contentDescription = "Back",
-                    tint = Color.Black
+                // Speech bubble
+                Box(
+                    modifier = Modifier
+                        .width(300.dp)
+                        .height(230.dp)
+                        .padding(16.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                        .background(Color(0xFFE8F5E9))
+                        .padding(16.dp)
+                ) {
+                    Text(text = message, fontSize = 18.sp, color = Color(0xFF4CAF50))
+                }
+            }
+
+            Image(
+                painter = painterResource(id = R.drawable.bird_img),
+                contentDescription = "Bird Image",
+                modifier = Modifier
+                    .height(250.dp)
+                    .width(250.dp)
+                    .align(Alignment.Start)
+            )
+
+            // Icon buttons section
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                IconButton(onClick = { message = getMessage("self") }) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_self), // Replace with your image resource
+                        contentDescription = "Self",
+                        modifier = Modifier.size(26.dp)
+                    )
+                }
+                IconButton(onClick = { message = getMessage("study") }) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_study), // Replace with your image resource
+                        contentDescription = "Study",
+                        modifier = Modifier.size(26.dp)
+                    )
+                }
+                IconButton(onClick = { message = getMessage("relationship") }) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_relationship), // Replace with your image resource
+                        contentDescription = "Relationship",
+                        modifier = Modifier.size(26.dp)
+                    )
+                }
+                IconButton(onClick = { message = getMessage("emotion") }) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_emotion), // Replace with your image resource
+                        contentDescription = "Emotion",
+                        modifier = Modifier.size(26.dp)
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(2.dp))
+
+            // Text box below icons
+            Box(
+                modifier = Modifier
+                    .wrapContentWidth()
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(Color(0xFFE8F5E9)) // Light green background
+                    .padding(vertical = 12.dp, horizontal = 16.dp) // Adjust padding as needed
+                    .align(Alignment.CenterHorizontally)
+            ) {
+                Text(
+                    text = "Select things to talk about",
+                    color = Color(0xFF4CAF50), // Green text color
+                    fontSize = 16.sp
                 )
             }
 
-            // Speech bubble
-            Box(
+            Spacer(modifier = Modifier.height(6.dp))
+
+            // Profile Section Header
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            ) {
+                Text(
+                    text = "Profile",
+                    style = MaterialTheme.typography.titleMedium.copy(fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                )
+            }
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            // Profile Details
+            Column(
                 modifier = Modifier
-                    .width(300.dp)
-                    .padding(16.dp)
-                    .clip(RoundedCornerShape(16.dp))
-                    .background(Color(0xFFE8F5E9))
+                    .fillMaxWidth()
+                    .background(Color.White, RoundedCornerShape(16.dp))
                     .padding(16.dp)
             ) {
-                Text(text = "How are you today?", fontSize = 18.sp, color = Color(0xFF4CAF50))
+                birdNameDescPair?.let {
+                    ProfileDetailItem(label = "Breed", value = it.first,
+                        detail = it.second)
+                }
+                ProfileDetailItem(label = "Age", value = "27 Days")
+                ProfileDetailItem(label = "Weight", value = "20g")
+                ProfileDetailItem(label = "Height", value = "12cm")
             }
-        }
-
-        Image(
-            painter = painterResource(id = R.drawable.bird_img),
-            contentDescription = "Bird Image",
-            modifier = Modifier
-                .height(250.dp)
-                .width(250.dp)
-                .align(Alignment.Start)
-        )
-
-        // Icon buttons section
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            IconButton(onClick = { /* Handle click */ }) {
-                Icon(Icons.Default.MailOutline, contentDescription = "Book", tint = Color(0xFF4CAF50))
-            }
-            IconButton(onClick = { /* Handle click */ }) {
-                Icon(Icons.Default.Face, contentDescription = "Face", tint = Color(0xFF4CAF50))
-            }
-            IconButton(onClick = { /* Handle click */ }) {
-                Icon(Icons.Default.Home, contentDescription = "Home", tint = Color(0xFF4CAF50))
-            }
-            IconButton(onClick = { /* Handle click */ }) {
-                Icon(Icons.Default.FavoriteBorder, contentDescription = "Group", tint = Color(0xFF4CAF50))
-            }
-        }
-
-        Spacer(modifier = Modifier.height(2.dp))
-
-        // Text box below icons
-        Box(
-            modifier = Modifier
-                .wrapContentWidth()
-                .clip(RoundedCornerShape(16.dp))
-                .background(Color(0xFFE8F5E9)) // Light green background
-                .padding(vertical = 12.dp, horizontal = 16.dp) // Adjust padding as needed
-                .align(Alignment.CenterHorizontally)
-        ) {
-            Text(
-                text = "Select things to talk about",
-                color = Color(0xFF4CAF50), // Green text color
-                fontSize = 16.sp
-            )
-        }
-
-        Spacer(modifier = Modifier.height(6.dp))
-
-        // Profile Section Header
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(horizontal = 16.dp)
-        ) {
-            Text(
-                text = "Profile",
-                style = MaterialTheme.typography.titleMedium.copy(fontSize = 20.sp, fontWeight = FontWeight.Bold)
-            )
-        }
-
-        Spacer(modifier = Modifier.height(12.dp))
-
-        // Profile Details
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(Color.White, RoundedCornerShape(16.dp))
-                .padding(16.dp)
-        ) {
-            birdNameDescPair?.let {
-                ProfileDetailItem(label = "Breed", value = it.first,
-                    detail = it.second)
-            }
-            ProfileDetailItem(label = "Age", value = "27 Days")
-            ProfileDetailItem(label = "Weight", value = "20g")
-            ProfileDetailItem(label = "Height", value = "12cm")
         }
     }
 }
@@ -234,6 +244,7 @@ fun ProfileDetailItem(label: String, value: String, detail: String = "") {
         Column {
             Text(text = value, style = MaterialTheme.typography.bodyLarge)
             if (detail != "") {
+                Spacer(modifier = Modifier.height(2.dp))
                 Text(text = detail, style = MaterialTheme.typography.bodyMedium)
             }
         }
@@ -252,4 +263,10 @@ fun getBirdNameAndDescription(lat: Double, lon: Double): Pair<String, String> {
     return Pair("Northern Cardinal",
                 "Striking and familiar backyard bird throughout most of eastern North America. " +
                 "Crest, large red bill, and long tail.") // default bird
+}
+
+fun getMessage(category: String): String {
+    val filteredMessages = messages.filter { message ->  message.category == category }
+    val i = Random.nextInt(10)
+    return filteredMessages[i].message
 }
